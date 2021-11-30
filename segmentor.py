7f2f02027d6b62dc16d8b386ca2ad02e28bfe0ce
hdawson1998 <78324320+hdawson1998@users.noreply.github.com>
2021-11-30 18:08:43 -0500

    changed init, call

--- segmentor.py
+++ segmentor.py
@@ -5,49 +5,45 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
 
 class Segmentor(tf.keras.Model):
-	def __init__(self):
+    def __init__(self):
+        super(Segmentor, self).__init__()
+        self.conv_down1 = Sequential([Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      MaxPooling2D(pool_size=(2, 2))])
+        self.conv_down2 = Sequential([Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      Dropout(0.5),
+                                      MaxPooling2D(pool_size=(2, 2))])
+        self.bottom = Sequential([Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                      UpSampling2D(size=(2, 2))])
+        #this net needs to concat w/ bottom output and down2 output
+        self.conv_up1 = Sequential([Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                    Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                    UpSampling2D(size=(2, 2))])
+        self.conv_up2 = Sequential([Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                    Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal'),
+                                    Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+                                    Conv2D(1, 1, activation='sigmoid')])
 
-		super(Segmentor, self).__init__()
-		inputs = Input(input_size)
-        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-        drop2 = Dropout(0.5)(conv2)
-        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    @tf.function
+    def call(self, inputs):
+        down1 = self.conv_down1(inputs)
+        down2 = self.conv_down2(down1)
+        bottom = self.bottom(down2)
+        bottom_concat = concatenate([down2, bottom], axis=3)
+        up1 = self.conv_up1(bottom_concat)
+        up1_concat = concatenate([down1, up1], axis=3)
+        up2 = self.conv_up2(up1_concat)
+        return up2
 
-        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-        drop3 = Dropout(0.5)(conv3)
+    def accuracy_function(self):
 
-        up1 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(drop3))
-        merge1 = concatenate([drop2, up1], axis=3)
-        conv4 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge1)
-        conv4 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-
-        up2 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-            UpSampling2D(size=(2, 2))(conv4))
-        merge2 = concatenate([conv1, up2], axis=3)
-        conv5 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge2)
-        conv5 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-
-        conv6 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-        conv7 = Conv2D(1, 1, activation='sigmoid')(conv6)
-
-
-	@tf.function
-	def call(self):
-
-		pass
-
-	def accuracy_function(self):
-
-		pass
+        pass
 
 
-	def loss_function(self):
+    def loss_function(self):
 
-		return pass
+
+        return pass
 
